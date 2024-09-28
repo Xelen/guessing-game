88a103668e3f7c07980d6fff4df43ccf0be36e97
Xuanita <alabama10@inbox.lv>
2024-09-28 04:23:16 +0300

    feat: add guessing game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(start, end) {
+    this.start = start; // минимальное значение диапазона
+    this.end = end; // максимальное значение диапазона
+  }
 
-    }
+  guess() {
+    this.idea = Math.floor((this.end + this.start) / 2 + 0.5); // среднее значение и округляем
+    return this.idea; // текущее предположение
+  }
 
-    guess() {
+  lower() {
+    return (this.end = this.idea); // если предположение слишком высокое, обновляем максимальное значение
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    return (this.start = this.idea); // если предположение слишком низкое, обновляем минимальное значение
+  }
 }
 
 module.exports = GuessingGame;
